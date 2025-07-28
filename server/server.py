a72ae6031adb2a826a035268f7ff7dbbd1e3f872
raghavahuja2801 <raghavahuja2801@gmail.com>
2025-07-28 15:41:06 -0700

    updated lock/release functionality

--- server/server.py
+++ server/server.py
@@ -26,6 +26,10 @@ class Server:
         self.server_socket.listen(5)
         self.is_running = False
 
+        # Track connected clients and object locks
+        self.clients = []  # List of (client_socket, client_address)
+        self.locked_objects = {}  # object_id -> client_address
+
     def start(self):
         """
         Start the server and begin accepting client connections
@@ -40,6 +44,7 @@ class Server:
         try:
             while self.is_running:
                 client_socket, client_address = self.server_socket.accept()
+                self.clients.append((client_socket, client_address))
                 # Handle each client in a separate thread
                 client_thread = threading.Thread(
                     target=self.handle_client_connection, 
@@ -87,24 +92,35 @@ class Server:
                 received_data = client_socket.recv(BUFFER_SIZE)
                 if not received_data:
                     break
-                
                 try:
                     # Try to deserialize as protocol message
                     message = deserialize(received_data)
-                    response = self.handle_message(message, client_address)
+                    response, broadcast = self.handle_message(message, client_address)
                     if response:
                         client_socket.send(response)
+                    # If broadcast is set, send to all clients
+                    if broadcast:
+                        self.broadcast_to_clients(broadcast, exclude=client_socket)
                 except json.JSONDecodeError:
                     # If it's not a protocol message, echo it back (legacy behavior)
                     client_socket.send(received_data)
-                
         except ConnectionResetError:
             print(f"Client {client_address} disconnected unexpectedly")
         except Exception as error:
             print(f"Error handling client {client_address}: {error}")
         finally:
             client_socket.close()
+            # Remove from clients list
+            self.clients = [(sock, addr) for sock, addr in self.clients if sock != client_socket]
             print(f"Connection with {client_address} closed")
+
+    def broadcast_to_clients(self, message_bytes, exclude=None):
+        for sock, _ in self.clients:
+            if sock != exclude:
+                try:
+                    sock.send(message_bytes)
+                except Exception:
+                    pass
 
     def handle_message(self, message, client_address):
         """
@@ -112,15 +128,47 @@ class Server:
         """
         msg_type = message.get('type')
         payload = message.get('payload', {})
-        
+
         print(f"Received {msg_type} from {client_address}")
-        
+
+        # Default: (response, broadcast)
+        response = None
+        broadcast = None
+
         if msg_type == MSG_CREATE_ROOM:
-            return self.handle_host_game(payload, client_address)
+            response = self.handle_host_game(payload, client_address)
+        elif msg_type == MSG_PLAYER_INPUT:
+            action = payload.get('action')
+            object_id = payload.get('object_id')
+            if action == INPUT_ACTION_LOCK_OBJECT and object_id:
+                # Try to lock the object
+                if object_id not in self.locked_objects:
+                    self.locked_objects[object_id] = client_address
+                    # Broadcast lock
+                    lock_payload = {'object_id': object_id, 'locked_by': str(client_address)}
+                    broadcast = serialize(MSG_OBJECT_LOCKED, lock_payload)
+                    # Also send confirmation to the requester
+                    response = serialize(MSG_OBJECT_LOCKED, lock_payload)
+                else:
+                    # Already locked, send error
+                    response = serialize(MSG_ERROR, {'message': f'Object {object_id} is already locked.'})
+            elif action == INPUT_ACTION_RELEASE_OBJECT and object_id:
+                # Only the locker can release
+                if self.locked_objects.get(object_id) == client_address:
+                    del self.locked_objects[object_id]
+                    release_payload = {'object_id': object_id, 'released_by': str(client_address)}
+                    broadcast = serialize(MSG_OBJECT_RELEASED, release_payload)
+                    # Also send confirmation to the requester
+                    response = serialize(MSG_OBJECT_RELEASED, release_payload)
+                else:
+                    response = serialize(MSG_ERROR, {'message': f'You cannot release object {object_id}.'})
+            else:
+                response = serialize(MSG_ERROR, {'message': 'Invalid player input.'})
         else:
             # Unknown message type
-            error_response = serialize(MSG_ERROR, {'message': f'Unknown message type: {msg_type}'})
-            return error_response
+            response = serialize(MSG_ERROR, {'message': f'Unknown message type: {msg_type}'})
+
+        return response, broadcast
 
     def handle_host_game(self, payload, client_address):
         """
